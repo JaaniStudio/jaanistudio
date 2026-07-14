096751a023881a672b189aaf1579d5e888e6b354
huzaifacreates <huzaifashamsi819@gmail.com>
2026-07-14 11:33:53 +0500

    add: opengraph and done seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,13 +3,72 @@ import "./globals.css";
 import { Space_Grotesk, Inter, IBM_Plex_Mono } from 'next/font/google';
 import CustomCursor from './components/CustomCursor';
 
-export const metadata: Metadata = {
-  title: "JaaniStudio",
-  description: "Jaani Studio is a video editing and web dev services agency, based in Pakistan.",
-};
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 const plexMono = IBM_Plex_Mono({ subsets: ['latin'], weight: ['400', '500'], variable: '--font-mono' });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://www.jaanistudio.com/"),
+  title: "Jaani Studio",
+  description: "JaaniStudio is a creative digital agency offering full-stack web development, AI-powered solutions, video editing, and graphic design to help businesses build, grow, and stand out online.",
+
+  keywords: [
+    "personal branding",
+    "personal brand development",
+    "digital branding agency",
+    "web design",
+    "web development",
+    "conversion copywriting",
+    "copywriting services",
+    "SEO for personal brands",
+    "SEO optimization",
+    "branding and SEO services",
+    "branding studio",
+    "Vistazo Studio",
+    "seo",
+    "designing",
+    "UI/UX",
+    "hire personal branding agency",
+    "all in one branding and web agency",
+    "build a personal brand that stands out",
+    "video editing",
+    "graphic designing",
+    "videography",
+    "ad shoot",
+    "videos",
+    "video",
+    "reel making",
+    "reel editing",
+    'long-form videos',
+    "short-form videos",
+    "animation"
+  ],
+
+  openGraph: {
+    title: "Jaani Studio",
+    description:
+      "Helping you !",
+    url: "https://www.jaanistudio.com/",
+    siteName: "Jaani Studio",
+    images: [
+      {
+        url: "/logo.png",
+        width: 1097,
+        height: 1097,
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  twitter: {
+    card: "summary_large_image",
+    title: "Jaani Studio",
+    description:
+      "JaaniStudio is a creative digital agency offering full-stack web development, AI-powered solutions, video editing, and graphic design to help businesses build, grow, and stand out online.",
+    images: ["/logo.png"],
+  },
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
